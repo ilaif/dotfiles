6e22f125292c787ab745c8980d0014253434dd8f
ilaif <ifallach@gmail.com>
2024-09-01 10:26:11 +0300

    chore: devbox no-double install

--- mac-setup.sh
+++ mac-setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-vecho() {
+echox() {
   printf "[\033[1;32mmac-setup\033[0m] %s\n" "$1"
 }
 question() {
@@ -21,65 +21,65 @@ question() {
   done
 }
 
-vecho "#########################"
-vecho "# Welcome to MAC setup! #"
-vecho "#########################"
+echox "#########################"
+echox "# Welcome to MAC setup! #"
+echox "#########################"
 echo ""
 
-vecho "Updating OSX. If this requires a restart, run the script again."
+echox "Updating OSX. If this requires a restart, run the script again."
 sudo softwareupdate -ia
 
-vecho "Configuring macOS"
+echox "Configuring macOS"
 
-vecho "Faster key repeat"
+echox "Faster key repeat"
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 1
 
-vecho "Require password as soon as screensaver or sleep mode starts"
+echox "Require password as soon as screensaver or sleep mode starts"
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-vecho "Show filename extensions by default"
+echox "Show filename extensions by default"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-vecho "Enable tap-to-click"
+echox "Enable tap-to-click"
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-vecho "Disable natural scroll"
+echox "Disable natural scroll"
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
-vecho "Disable smart quotes"
+echox "Disable smart quotes"
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
-vecho "Disable smart dashes"
+echox "Disable smart dashes"
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
-vecho "Enable full keyboard access for all controls"
+echox "Enable full keyboard access for all controls"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-vecho "Automatically hide and show the Dock"
+echox "Automatically hide and show the Dock"
 defaults write com.apple.dock autohide -bool true
 
-vecho "Make showing the dock faster"
+echox "Make showing the dock faster"
 defaults write com.apple.Dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0.4
 killall Dock
 
-vecho "Make window resizing animations faster"
+echox "Make window resizing animations faster"
 defaults write -g NSWindowResizeTime -float 0.003
 
-vecho "Disable opening and closing window animations"
+echox "Disable opening and closing window animations"
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 
-vecho "Only use UTF-8 in Terminal.app"
+echox "Only use UTF-8 in Terminal.app"
 defaults write com.apple.terminal StringEncodings -array 4
 
-vecho "Installing Xcode Command Line Tools"
+echox "Installing Xcode Command Line Tools"
 xcode-select --install || true
 
 echo ""
-vecho "Mac setup complete ✅"
+echox "Mac setup complete ✅"
 echo ""
 
 DO_LOGOUT=n
